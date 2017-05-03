4397925d960930fb241fa6a60bf58e2a4eb78c52
Pavel Penchev <pavel.penchev@gmail.com>
2017-05-03 12:45:42 +0300

    resolve #2 : Extract static references to QueryExecutor, further code re-organization

---
+++ frontend/js/ui/result-renderer.js
@@ -0,0 +1,68 @@
+var ResultRenderer = {
+	
+	"create" : function(args) {
+		var queryExecutor = args.queryExecutor;
+		var facetsContainer = args.facetsContainer;
+		var countContainer = args.countContainer;
+		var itemsContainer = args.itemsContainer;
+		var detailsContainer = args.detailsContainer;
+		
+		return {
+			"render" : function(result) {
+				if (!result) {
+					return;
+				}
+
+				var catalog = result.catalog;
+
+				FacetRenderer.renderFacets({
+					"container" : facetsContainer,
+					"facets" : result.facets,
+					"catalog" : catalog,
+					"clickCallback" : this.render,
+					"queryExecutor" : queryExecutor
+				});
+
+				// Render the items
+				countContainer.empty();
+				var listerSize = result.catalog.listerSize || 40;
+				if (result.count > listerSize) {
+					countContainer.append("Showing " + listerSize + " out of " + result.count + " results");
+				} else {
+					countContainer.append("Showing " + result.count + " results");
+				}
+				
+				var saveCallback = function() {
+					detailsContainer.empty().hide();
+					itemsContainer.fadeIn();
+				};
+				
+				// TODO Extract as a separate file
+				var renderDetails = function(item, catalog) {
+					ItemRenderer.renderDetails({
+						"container" : detailsContainer,
+						"item" : item,
+						"catalog" : catalog,
+						"saveCallback" : saveCallback,
+						"cancelCallback" : saveCallback
+					});
+
+					itemsContainer.hide();
+					detailsContainer.fadeIn();					
+				}
+				
+				var container = $(document.createElement("div"));
+				ItemRenderer.renderList({
+					"container" : container,
+					"items" : result.items,
+					"fields" : result.fields,
+					"catalog" : catalog,
+					"clickListener" : renderDetails
+				});
+
+				itemsContainer.empty();
+				itemsContainer.append(container);
+			}
+		}
+	}
+}
