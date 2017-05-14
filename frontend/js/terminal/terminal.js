7eade95430adc5463dc9e842ac9f13db778cfd61
Pavel Penchev <pavel.penchev@gmail.com>
2017-05-14 23:45:18 +0300

    Issue #4 Add basic command line parsing and help messages, use cd to add filtering steps to the query object

--- frontend/js/terminal/terminal.js
+++ frontend/js/terminal/terminal.js
@@ -41,17 +41,72 @@ var Terminal = {
       var queryObject = Util.parseQueryString();
   		var q = new Query(queryObject.q);
 
-  		var term = command; // strip the leading s
-  		if (term) {
-  			q.setSearch(term);
-  			queryObject.q = q.toString();
-  		} else if (q.hasSelection("$s")) {
-  			q.removeSelection("$s");
-  			queryObject.q = q.toString();
-  		}
+      var c = this.parseCommand(command);
+      if (c.command == "h" || c.command == "help") {
+        this.printHelp(terminal);
+      } else if (c.command == "s") {
+          var term = c.args.join(" ");
 
-  		queryExecutor.navigate("?" + Util.queryObjectToString(queryObject), function(result) {
-        terminal.echo("Showing " + result.count + " results");
-  		});
+          if (term.length > 0) {
+      			q.setSearch(term);
+      			queryObject.q = q.toString();
+      		} else if (q.hasSelection("$s")) {
+      			q.removeSelection("$s");
+      			queryObject.q = q.toString();
+      		}
+
+          queryExecutor.navigate("?" + Util.queryObjectToString(queryObject), function(result) {
+            terminal.echo("Showing " + result.count + " results");
+          });
+      } else  if (c.command == "cd") {
+        if (c.args[0] == ".." || c.args[0] == "../") {
+          q.removeSelectionAt(q.getSelections().length - 1);
+          queryObject.q = q.toString();
+
+          queryExecutor.navigate("?" + Util.queryObjectToString(queryObject), function(result) {
+            terminal.echo("Showing " + result.count + " results");
+          });
+        } else {
+          q.addSelection(c.args[0], "=", c.args[1]);
+          queryObject.q = q.toString();
+
+          queryExecutor.navigate("?" + Util.queryObjectToString(queryObject), function(result) {
+            terminal.echo("Showing " + result.count + " results");
+          });
+        }
+      }
+  },
+
+  "printHelp" : function(terminal) {
+    terminal.echo("Usage:");
+    terminal.echo("\"s keywords\"\tSearch for the specified keywords within the catalog of documents");
+    terminal.echo("\"cd field value\"\tFilter the result with the specified value for the given field");
+    terminal.echo("\"cd ../\"\tRemove the last filtering step");
+    terminal.echo();
+  },
+
+  "parseCommand" : function(command) {
+    var split = [];
+    var buffer = "";
+
+    for (var i = 0; i < command.length; i++) {
+        var char = command[i];
+        if (char == ' ' && buffer.length > 0) {
+          split.push(buffer);
+          buffer = "";
+        } else {
+          buffer += char;
+        }
+    }
+
+    if (buffer.length > 0) {
+        split.push(buffer);
+    }
+
+    var c = split.shift();
+    return {
+      "command" : c,
+      "args" : split
+    };
   }
 }
