139ce70f06ef80e4c9e7f3211e0061d0494ebcae
Ivan Zamanov <izamanov@fredhopper.com>
2017-07-03 09:33:56 +0300

    Refactor how document fields are structured in the back end, need to update the front end as well

--- js/backend/model.js
+++ js/backend/model.js
@@ -1,3 +1,5 @@
+let marked = require('marked');
+
 function toEntryName(id) {
   let split = id.split('/');
   return split[split.length - 1];
@@ -55,9 +57,75 @@ class BaseCatalogOperations {
   }
 }
 
+class AnalyzedItem {
+  constructor(id, rawContent) {
+    this.id = id;
+    this.setContent(rawContent);
+  }
+
+  setContent(rawContent) {
+    this.rawContent = rawContent;
+    let lexer = new marked.Lexer(),
+        lexemes = lexer.lex(rawContent),
+        topHeadings = [];
+
+    // Gather top-level headings, these will be considered
+    // for field names, i.e. facet names
+    lexemes.forEach((lexeme, index) => {
+      let isTopLevelHeading = lexeme.type === 'heading' &&
+        lexeme.depth === 1 &&
+        topHeadings.indexOf(lexeme.text) < 0;
+
+      if(isTopLevelHeading) {
+        topHeadings.push({
+          text: lexeme.text.toLowerCase(),
+          index: index
+        });
+      }
+    });
+
+    // Gather text values for each top-level heading,
+    // these will be consiedered for field values, i.e. facet values
+    this.fields = [];
+    topHeadings.forEach((heading, index) => {
+      let nextHeading = topHeadings[index + 1];
+      let valueLexemes = lexemes.slice(heading.index + 1,
+                                       (nextHeading && nextHeading.index) || lexemes.length);
+      this.fields.push(new DocumentField(heading.text, valueLexemes));
+    });
+  }
+
+  field(name) {
+    return this.fields.find((f) => f.name === name);
+  }
+}
+
+class DocumentField {
+  constructor(name, valueLexemes) {
+    this.name = name;
+    this.valueLexemes = valueLexemes;
+  }
+
+  get textValue() {
+    // TODO: Improve how the text value is collected
+    return this.valueLexemes.map((l) => l.text || '').join(' ');
+  }
+
+  get lexemes() {
+    return this.valueLexemes;
+  }
+}
+
+class DisplayItem extends AnalyzedItem {
+  constructor(id, rawContent) {
+    super(id, rawContent);
+  }
+}
+
 module.exports = {
   CatalogItemEntry: CatalogItemEntry,
   CatalogItem: CatalogItem,
   BaseCatalogOperations: BaseCatalogOperations,
+  AnalyzedItem: AnalyzedItem,
   toEntryName: toEntryName
 };
--- test/indexing.js
+++ test/indexing.js
@@ -27,5 +27,37 @@ module.exports = {
     } catch(e) { console.log(e); test.fail(); }
 
     test.done();
+  },
+  itemAnalysis: function(test) {
+    let md = `
+# Name
+Publishing UI improvements
+
+# Description
+This is sentence 1.
+This is sentence 2.
+
+# Empty
+
+# Some List
+- item1
+- item2
+`;
+
+    let analyzed = new model.AnalyzedItem(null, md);
+    test.deepEqual(analyzed.fields.map((field) => field.name),
+                   ['name', 'description', 'empty', 'some list']);
+
+    let hasFieldValue = (fieldName, value) =>
+        test.equal(
+          analyzed.fields.find((f) => f.name === fieldName).textValue,
+          value);
+
+    hasFieldValue('empty', '');
+    hasFieldValue('name', 'Publishing UI improvements');
+    hasFieldValue('description', `This is sentence 1.
+This is sentence 2.`);
+    // TODO: plain lists...
+    test.done();
   }
 };
