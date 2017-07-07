b770777a4daf2cff7ffa7dbeb17935aca90ce85c
Pavel Penchev <pavel.penchev@gmail.com>
2017-07-07 09:34:56 +0300

    Use new dropbox client id, user is rebulasap@gmail.com

--- js/repository/repository-controller.js
+++ js/repository/repository-controller.js
@@ -1,4 +1,4 @@
-const rebulasDropboxClientID = "jimrv9o4n8d3yyj";
+const rebulasDropboxClientID = "ot4wauixsfog5px";
 
 // Register on-going OAuth sessions for mapping to OAuth callbacks
 window.OAuthSessions = [];
